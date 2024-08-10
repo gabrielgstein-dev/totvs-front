adc94719709796e8eaf1d3b53f2f62c180f112ab
Gabriel Stein <gabriel.g.stein@gmail.com>
2024-08-10 08:19:15 -0300

    feat: Add utility function for formatting CPF and CNPJ numbers

---
+++ src/app/shared/utils/formatCpfCnpj.ts
@@ -0,0 +1,12 @@
+export const formatCpfCnpj = (value: string): string => {
+  if (value.length === 11) {
+    return value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  } else if (value.length === 14) {
+    return value.replace(
+      /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+      '$1.$2.$3/$4-$5'
+    );
+  } else {
+    return value;
+  }
+};
